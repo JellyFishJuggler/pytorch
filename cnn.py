96d09bcafb6f22ae31c7541cd30cb85bdc827b0e
JellyFishJuggler <guptasrijan85@gmail.com>
2026-09-19 13:06:54 +0530

    cnn fundamental

--- cnn.py
+++ cnn.py
@@ -1,5 +1,15 @@
 import torch
 import torch.nn as nn
+import torch.optim as op
+from torch.utils.data import Dataset, DataLoader
+# from PIL import Image
+# from torchvision import transforms
+
+
+# image = Image.open('digit.png')
+# transform = transforms.ToTensor()
+# x = transform(image)
+# print(x.shape)
 
 class CNN(nn.Module):
     
@@ -12,22 +22,57 @@ class CNN(nn.Module):
     
     def forward(self, x):
         x = self.conv(x)
-        print("conv:", x.shape)
-    
+        # print("conv:", x.shape)
         x = self.relu(x)
-        print("relu:", x.shape)
-    
+        # print("relu:", x.shape)
         x = self.pool(x)
-        print("pool:", x.shape)
-    
+        # print("pool:", x.shape)
         x = torch.flatten(x, 1)
-        print("flatten:", x.shape)
-    
+        # print("flatten:", x.shape)
         return self.fc(x)
+    
+
+class myDataset(Dataset):
+
+    def __init__(self, X, y):
+        super().__init__()
+        self.X = X
+        self.y = y
+    
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
 
 
-x = torch.randn(1, 1, 28, 28)
+X = torch.randn(30, 1, 28, 28)
+y = torch.randint(0, 3, (30,))
+
+dataset = myDataset(X,y)
+dataloader = DataLoader(dataset, batch_size=5, shuffle=True)
+
 model = CNN()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = op.SGD(model.parameters(), lr=0.01)
 
-y = model(x)
-print(y)
+epochs = 500
+for epoch in range(epochs):
+    for x_i, y_i in dataloader:
+        
+        optimizer.zero_grad()
+        y_i_predict = model(x_i)
+        loss = loss_fn(y_i_predict, y_i)
+        loss.backward()
+        optimizer.step()
+        
+        
+model.eval()
+with torch.no_grad():
+    
+    logits = model(X)
+    ypred = torch.argmax(logits, dim=1)
+    loss = loss_fn(logits,y)
+    
+    print(f'Test Loss {loss}')
+    print(f'Predictions: {ypred} | logits: {logits}')
